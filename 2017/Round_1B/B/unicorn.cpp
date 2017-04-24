8f3dc9213d26f416e4ad4c6e014f572412610e0e
Mario Apra <mario_surfing@hotmail.com>
2017-04-23 21:11:44 -0300

    remove unused

--- 2017/Round_1B/B/unicorn.cpp
+++ 2017/Round_1B/B/unicorn.cpp
@@ -15,22 +15,8 @@ typedef vector<ii>			vii;
 #define forr(it,container)  for(auto it=container.begin(); it!=container.end(); it++)
 #define w(t)				int t;si(t);while(t--)
 
-typedef struct un_color{
-    char name;
-    char col1;
-    char col2;
-    vector<char> allowed;
-} un_color;
 
-pair<char, int> maxCh(map<char, int> *un, un_color atual){
-    pair<char, int> resp = make_pair('Z', -1);
-    for (map<char, int>::iterator it = (*un).begin(); it != (*un).end(); it++){
-        //if (it->second > resp.second && it->first != restr)
-            resp = make_pair(it->first, it->second);
-    }
 
-    return resp;
-}
 
 void clear_map(map<char, int> *un){
     for(map<char,int>::iterator it = (*un).begin(); it != (*un).end(); it++){
@@ -39,89 +25,7 @@ void clear_map(map<char, int> *un){
     }
 }
 
-void print_map(map<char, int> *un){
-    for(map<char,int>::iterator it = (*un).begin(); it != (*un).end(); it++){
-        cout << it->first << ", " << it->second << "\n";
-    }
-    cout << "\n";
-}
-
-
-vector<un_color> get_colors(){
-    vector<un_color> colors;
-    vector<char> allowed;
-
-    // Orange = R + Y
-    un_color col;
-    col.name = 'O';
-    col.col1 = 'R';
-    col.col2 = 'Y';
-    allowed.push_back('B');
-    col.allowed = allowed;
-
-    colors.push_back(col);
-
-    // Green = Y + B
-    col.name = 'G';
-    col.col1 = 'Y';
-    col.col2 = 'B';
-    allowed.clear();
-    allowed.push_back('R');
-    col.allowed = allowed;
-
-    colors.push_back(col);
-
-    // Violet = R + B
-    col.name = 'V';
-    col.col1 = 'R';
-    col.col2 = 'B';
-    allowed.clear();
-    allowed.push_back('Y');
-    col.allowed = allowed;
-
-    colors.push_back(col);
-
-    // Red
-    col.name = 'R';
-    col.col1 = 'R';
-    col.col2 = 'R';
-    allowed.clear();
-    allowed.push_back('G');
-    allowed.push_back('Y');
-    allowed.push_back('B');
-    col.allowed = allowed;
-
-    colors.push_back(col);
-
-
-    // Yellow
-    col.name = 'Y';
-    col.col1 = 'Y';
-    col.col2 = 'Y';
-    allowed.clear();
-    allowed.push_back('V');
-    allowed.push_back('R');
-    allowed.push_back('B');
-    col.allowed = allowed;
-
-    colors.push_back(col);
-
-    // Blue
-    col.name = 'B';
-    col.col1 = 'B';
-    col.col2 = 'B';
-    allowed.clear();
-    allowed.push_back('O');
-    allowed.push_back('R');
-    allowed.push_back('Y');
-    col.allowed = allowed;
-
-    colors.push_back(col);
-
-    return colors;
-}
-
-pair<char, int> next_small(map<char, int> *un, pair<char, int> actual, pair<char, int> first){
+pair<char, int> next_small(map<char, int> *un, pair<char, int> actual, char fst){
     clear_map(un);
     pair<char,int> rtn = make_pair('Z', -1);
 
